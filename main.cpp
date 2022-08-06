af763203a451fd4f9820515085b72b210b5b4130
Kostas Zafolias <zafoliaskostas@gmail.com>
2022-08-06 17:08:02 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,38 +1,109 @@
-/******************************************************************************
-
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include <iostream>
-
 using namespace std;
-
-int main()
-{
-    
-    cout<<"pernas ta mathimatika? lmao a gumnasou ";
-    cout<<"grapse tousbathoums a tetramhnou ";
+int main() {
+	
+	cout<<"pernas ta mathimatika? lmao a gumnasou ";
+    cout<<"grapse tous bathoums a tetramhnou ";
     cout<<"grapse mathimatika";
     int mathimatikaa;
     cin>> mathimatikaa;
-    cout<<"grapse tousbathoums b tetramhnou ";
+    cout<<"grapse tous bathoums b tetramhnou ";
     cout<<"grapse mathimatika";
     int mathimatikab;    
     cin>> mathimatikab;
-    cout<<"grapse tousbathoums exetaseis ";
+    cout<<"grapse tous bathoums exetaseis ";
     cout<<"grapse mathimatika";
     int mathimatikae;   
     cin>> mathimatikae;
-    if(mathimatikae+mathimatikaa+mathimatikab > 29) {
-        
-         cout<<"pernas";
-    }
+
+    cout<<"pernas ta gallika/germanika? lmao a gumnasou ";
+    cout<<"grapse tous bathoums a tetramhnou ";
+    int gallgerma;
+    cin>> gallgerma;
+    cout<<"grapse tous bathoums b tetramhnou ";
+
+    int gallgermb;    
+    cin>> gallgermb;
+    cout<<"grapse tous bathoums exetaseis ";
+    int gallgerme;   
+    cin>> gallgerme;
     
-    else {
-        cout << "den pernas math";
+    
+    cout<<"pernas ta agglika? lmao a gumnasou ";
+    cout<<"grapse tous bathoums a tetramhnou ";
+    int aggla;
+    cin>> aggla;
+    cout<<"grapse tous bathoums b tetramhnou ";
+
+    int agglb;    
+    cin>> agglb;
+    cout<<"grapse tous bathoums exetaseis ";
+    int aggle;   
+    cin>> aglle;
+    
+    cout<<"pernas ergastirio dexiotitwn? lmao a gumnasou ";
+    cout<<"grapse tous bathoums a tetramhnou ";
+    int erga;
+    cin>> erga;
+    cout<<"grapse tous bathoums b tetramhnou ";
+
+    int ergb;    
+    cin>> ergb;
+    cout<<"grapse tous bathoums exetaseis ";
+    int erge;   
+    cin>> erge;
+    
+    cout<<"pernas ergastirio eikastika/kalitexnika? lmao a gumnasou ";
+    cout<<"grapse tous bathoums a tetramhnou ";
+    int kala;
+    cin>> kala;
+    cout<<"grapse tous bathoums b tetramhnou ";
+
+    int kalb;    
+    cin>> kalb;
+    cout<<"grapse tous bathoums exetaseis ";
+    int kale;   
+    cin>> kale;
+    
+    cout<<"pernas mousiki? lmao a gumnasou ";
+    cout<<"grapse tous bathoums a tetramhnou ";
+    int musa;
+    cin>> musa;
+    cout<<"grapse tous bathoums b tetramhnou ";
+
+    int musb;    
+    cin>> musb;
+    cout<<"grapse tous bathoums exetaseis ";
+    int muse;   
+    cin>> muse;
+    
+    
+    cout<<"pernas technologia? lmao a gumnasou ";
+    cout<<"grapse tous bathoums a tetramhnou ";
+    int techa;
+    cin>> techa;
+    cout<<"grapse tous bathoums b tetramhnou ";
+
+    int techb;    
+    cin>> techb;
+    cout<<"grapse tous bathoums exetaseis ";
+    int teche;   
+    cin>>teche;
+	return 0;
+
+    cout<<"pernas plhroforikh? lmao a gumnasou ";
+    cout<<"grapse tous bathoums a tetramhnou ";
+    int plhra;
+    cin>> plhra;
+    cout<<"grapse tous bathoums b tetramhnou ";
+
+    int plhrb;    
+    cin>> plhrb;
+    cout<<"grapse tous bathoums exetaseis ";
+    int plhre;   
+    cin>> plhre;
+
+}
     }
     
      cout<<"pernas ta gallika/germanika? lmao a gumnasou ";
