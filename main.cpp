44651361d662d2dbfcd19e2844f71e2ba32044d2
Kostas Zafolias <zafoliaskostas@gmail.com>
2022-08-06 05:31:23 -0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,60 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+    cout<<"pernas ta mathimatika? lmao a gumnasou ";
+    cout<<"grapse tousbathoums a tetramhnou ";
+    cout<<"grapse mathimatika";
+    int mathimatikaa;
+    cin>> mathimatikaa;
+    cout<<"grapse tousbathoums b tetramhnou ";
+    cout<<"grapse mathimatika";
+    int mathimatikab;    
+    cin>> mathimatikab;
+    cout<<"grapse tousbathoums exetaseis ";
+    cout<<"grapse mathimatika";
+    int mathimatikae;   
+    cin>> mathimatikae;
+    if(mathimatikae+mathimatikaa+mathimatikab > 29) {
+        
+         cout<<"pernas";
+    }
+    
+    else {
+        cout << "den pernas math";
+    }
+    
+     cout<<"pernas ta gallika/germanika? lmao a gumnasou ";
+    cout<<"grapse tousbathoums a tetramhnou ";
+    int gallgerma;
+    cin>> gallgerma;
+    cout<<"grapse tousbathoums b tetramhnou ";
+
+    int gallgermb;    
+    cin>> gallgermb;
+    cout<<"grapse tousbathoums exetaseis ";
+    cout<<"grapse mathimatika";
+    int mathimatikae;   
+    cin>> mathimatikae;
+    if(mathimatikae+mathimatikaa+mathimatikab > 29) {
+        
+         cout<<"pernas";
+    }
+    
+    else {
+        cout << "den pernas math";
+    }   
+    
+    return 0;
+}
